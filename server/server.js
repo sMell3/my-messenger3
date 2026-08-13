eb67b0175724c1888069fc3febaf550fbdaab56b
sMell3 <officialsteam@list.ru>
2026-08-13 20:23:02 +0300

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -2,120 +2,212 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const path = require("path");
 
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.static("../public"));
+
+// папка сайта
+app.use(
+    express.static(
+        path.join(__dirname, "../public")
+    )
+);
+
 
 
 const server = http.createServer(app);
 
 
+
 const io = new Server(server, {
+
     cors: {
-        origin: "*"
+        origin: "*",
+        methods: ["GET", "POST"]
     }
+
 });
 
 
-// список пользователей онлайн
+
+// пользователи онлайн
+
 let users = {};
 
 
 
+// подключение
+
 io.on("connection", (socket) => {
 
-    console.log("Подключился пользователь:", socket.id);
+
+    console.log(
+        "Подключился:",
+        socket.id
+    );
 
 
 
-    // вход по нику
-    socket.on("join", (nickname) => {
+    // вход
+
+    socket.on(
+        "join",
+        (nickname)=>{
 
 
-        users[socket.id] = nickname;
+            users[socket.id] = nickname;
 
 
-        io.emit("onlineUsers", Object.values(users));
+
+            io.emit(
+                "onlineUsers",
+                Object.values(users)
+            );
 
 
-        io.emit("message", {
-            user: "SERVER",
-            text: `${nickname} вошёл в чат`
-        });
+
+            io.emit(
+                "message",
+                {
+                    user:"SERVER",
+                    text:`${nickname} вошёл в чат`,
+                    time:new Date().toLocaleTimeString()
+                }
+            );
 
 
-    });
+        }
+    );
+
 
 
 
     // сообщение
-    socket.on("sendMessage", (text) => {
+
+    socket.on(
+        "sendMessage",
+        (text)=>{
 
 
-        let nickname = users[socket.id] || "Unknown";
-
-
-        io.emit("message", {
-
-            user: nickname,
-
-            text: text,
-
-            time: new Date().toLocaleTimeString()
-
-        });
-
-
-    });
+            let nickname =
+            users[socket.id] || "Unknown";
 
 
 
-    // выход
-    socket.on("disconnect", () => {
+            io.emit(
+                "message",
+                {
 
+                    user:nickname,
 
-        let nickname = users[socket.id];
+                    text:text,
 
+                    time:new Date().toLocaleTimeString()
 
-        delete users[socket.id];
+                }
+            );
 
-
-        io.emit("onlineUsers", Object.values(users));
-
-
-
-        if(nickname){
-
-            io.emit("message", {
-
-                user:"SERVER",
-
-                text:`${nickname} вышел из чата`
-
-            });
 
         }
-
-
-        console.log("Пользователь отключился");
-
-    });
-
-
-});
-
-
-
-
-
-server.listen(3000, () => {
-
-    console.log(
-        "Сервер запущен: http://localhost:3000"
     );
 
+
+
+
+
+    // отключение
+
+    socket.on(
+        "disconnect",
+        ()=>{
+
+
+            let nickname =
+            users[socket.id];
+
+
+
+            delete users[socket.id];
+
+
+
+            io.emit(
+                "onlineUsers",
+                Object.values(users)
+            );
+
+
+
+            if(nickname){
+
+
+                io.emit(
+                    "message",
+                    {
+
+                        user:"SERVER",
+
+                        text:`${nickname} вышел из чата`,
+
+                        time:new Date().toLocaleTimeString()
+
+                    }
+                );
+
+
+            }
+
+
+
+            console.log(
+                "Отключился:",
+                socket.id
+            );
+
+
+        }
+    );
+
+
 });
+
+
+
+
+
+// keep alive
+
+setInterval(()=>{
+
+    console.log(
+        "Server active"
+    );
+
+},60000);
+
+
+
+
+
+
+// Render сам выдаёт PORT
+
+const PORT =
+process.env.PORT || 3000;
+
+
+
+server.listen(
+    PORT,
+    ()=>{
+
+        console.log(
+            `Сервер запущен на порту ${PORT}`
+        );
+
+    }
+);
